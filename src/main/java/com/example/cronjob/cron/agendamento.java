e36c5d43466785ced2685819452a3a848ea8aa98
Jorge Luis <jorgeluis20022020@gmail.com>
2024-11-27 12:16:59 -0300

    first commit

---
+++ src/main/java/com/example/cronjob/cron/agendamento.java
@@ -0,0 +1,33 @@
+package com.example.cronjob.cron;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class agendamento {
+
+    public boolean isPrimo(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Scheduled(cron = "${spring.task.scheduling.cron}")
+    public void printNumeroPrimo() {
+        int count = 2;
+        System.out.println("Números primos:");
+
+        while (count < 100) {
+            if (isPrimo(count)) {
+                System.out.println(count);
+            }
+            count++;
+        }
+    }
+}
